dfc360403fdfd7cce71a9b0c1cc9489757657466
Goragod <admin@goragod.com>
2017-07-04 15:29:25 +0700

    update header

--- projects/api/modules/index/controllers/api.php
+++ projects/api/modules/index/controllers/api.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @filesource index/controllers/api.php
+ * @filesource modules/index/controllers/api.php
  * @link http://www.kotchasan.com/
  * @copyright 2016 Goragod.com
  * @license http://www.kotchasan.com/license/
--- projects/api/modules/index/models/api.php
+++ projects/api/modules/index/models/api.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @filesource index/models/api.php
+ * @filesource modules/index/models/api.php
  * @link http://www.kotchasan.com/
  * @copyright 2016 Goragod.com
  * @license http://www.kotchasan.com/license/
