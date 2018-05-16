ee09ddb56c820830eec570553ed975f83d091397
Goragod <admin@goragod.com>
2018-05-16 08:19:30 +0700

    fixed bugs

--- projects/api/index.php
+++ projects/api/index.php
@@ -12,13 +12,13 @@
  * 1 บันทึกข้อผิดพลาดและคำเตือนลง error_log .php
  * 2 แสดงผลข้อผิดพลาดและคำเตือนออกทางหน้าจอ (ใช้เฉพาะตอนออกแบบเท่านั้น)
  */
-//define('DEBUG', 0);
+define('DEBUG', 0);
 
 /**
  * false (default)
  * true บันทึกการ query ฐานข้อมูลลง log (ใช้เฉพาะตอนออกแบบเท่านั้น)
  */
-//define('DB_LOG', false);
+define('DB_LOG', false);
 // load Kotchasan
 include '../../Kotchasan/load.php';
 // Initial Kotchasan Framework
