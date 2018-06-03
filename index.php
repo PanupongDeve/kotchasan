37a30a5b1c66e0af3b675c4041987606475ec289
Goragod <admin@goragod.com>
2018-06-03 09:29:10 +0700

    format source

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * index.php
+ * index.php.
  *
  * @author Goragod Wiriya <admin@goragod.com>
- * @link http://www.kotchasan.com/
+ *
+ * @see http://www.kotchasan.com/
+ *
  * @copyright 2016 Goragod.com
  * @license http://www.kotchasan.com/license/
  */
-
 header('Location: projects/welcome/index.php');
