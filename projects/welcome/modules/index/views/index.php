bb950f0c7194293ff11b624a662edd7a7d47304e
Goragod <admin@goragod.com>
2019-06-14 15:31:45 +0700

    update Logo

--- projects/welcome/modules/index/views/index.php
+++ projects/welcome/modules/index/views/index.php
@@ -22,17 +22,14 @@ class View extends \Kotchasan\View
 {
     public function render()
     {
-        echo '<html style="height:100%;width:100%"><head>';
+        echo '<html><head>';
         echo '<meta charset=utf-8>';
         echo '<link href="https://fonts.googleapis.com/css?family=Itim&subset=thai,latin" rel="stylesheet" type="text/css">';
+        echo '<link href="modules/index/views/style.css" rel="stylesheet" type="text/css">';
         echo '<meta name=viewport content="width=device-width, initial-scale=1.0">';
-        echo '<style>';
-        echo '.warper{display:inline-block;text-align:center;height:50%;}';
-        echo '.warper::before{content:"";display:inline-block;height:100%;vertical-align:middle;width:0px;}';
-        echo '</style>';
         echo '</head><body style="height:100%;width:100%;margin:0;font-family:Itim, Tahoma, Loma;color:#666;">';
         echo '<div class=warper style="display:block"><div class="warper"><div>';
-        echo '<img src="kotchasan.png" style="width:100px" alt="Kotchasan PHP Framework">';
+        echo '<div class="elephant"><div class="body"></div><div class="tail"></div><div class="head"><div class="trunk"></div><div class="ear"></div><div class="eye"></div></div></div>';
         echo '<h1 style="line-height:1.8;margin:0;text-shadow:3px 3px 0 rgba(0,0,0,0.1);font-weight:normal;">คชสาร (Kotchasan)</h1>';
         echo 'Siam PHP Framework';
         echo '</div></div></body></html>';
